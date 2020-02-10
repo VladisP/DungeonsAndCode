4eb86bf69ac2eb1d6416b51fff3aba5860b1cf4a
Pichugin Vladislav <vlad19041999@yandex.ru>
2020-02-10 22:37:24 +0300

    [ADD] Переход на конструктор уровней со стартовой активити

--- app/src/main/java/ru/iu9/game/dungeonsandcode/activities/StartActivity.java
+++ app/src/main/java/ru/iu9/game/dungeonsandcode/activities/StartActivity.java
@@ -1,6 +1,8 @@
 package ru.iu9.game.dungeonsandcode.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +15,21 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        findViewById(R.id.open_constructor_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openConstructor();
+            }
+        });
+    }
+
+    private void openConstructor() {
+        startActivity(new Intent(StartActivity.this, ConstructorActivity.class));
     }
 }
